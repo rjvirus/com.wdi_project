fcd0dd27f26fc71ae8867304d9dea19f7338a7e9
rjvirus <rahulvirus100@gmail.com>
2020-11-27 16:32:18 +0100

    read schema mapping output files

--- src/main/java/IdentityResolution.java
+++ src/main/java/IdentityResolution.java
@@ -10,24 +10,15 @@ import java.io.File;
 import java.io.IOException;
 
 public class IdentityResolution {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws ParserConfigurationException, SAXException, XPathExpressionException, IOException {
 
         HashedDataSet<Player, Attribute> dataPredictionPlayers = new HashedDataSet<>();
         HashedDataSet<Player, Attribute> dataRealPlayers = new HashedDataSet<>();
         HashedDataSet<Player, Attribute> dataFifaPlayers = new HashedDataSet<>();
-        try {
-            new PlayerXMLReader().loadFromXML(new File("data/input/prediction_players.xml"),"/players/player", dataPredictionPlayers);
-            new PlayerXMLReader().loadFromXML(new File("data/input/real_market_players.xml"),"/players/player", dataRealPlayers);
-            new PlayerXMLReader().loadFromXML(new File("data/input/fifa_players.xml"),"/players/player", dataFifaPlayers);
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        } catch (SAXException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (XPathExpressionException e) {
-            e.printStackTrace();
-        }
+        new PlayerXMLReader().loadFromXML(new File("data/input/prediction_players.xml"),"/players/player", dataPredictionPlayers);
+        new PlayerXMLReader().loadFromXML(new File("data/input/real_market_players.xml"),"/players/player", dataRealPlayers);
+        new PlayerXMLReader().loadFromXML(new File("data/input/fifa_players.xml"),"/players/player", dataFifaPlayers);
+        
         System.out.println("Hello world");
     }
 }
