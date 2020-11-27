faede2e1ef4327feca8349061e5868c63330455a
rjvirus <rahulvirus100@gmail.com>
2020-11-27 16:03:45 +0100

    Initial commit

---
+++ src/main/java/model/Player.java
@@ -0,0 +1,222 @@
+package model;
+import de.uni_mannheim.informatik.dws.winter.model.AbstractRecord;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Player extends AbstractRecord<Attribute> implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    protected String id;
+    protected String provenance;
+    private String name;
+    private String nationality;
+    private LocalDateTime birth_date;
+    private String birth_place;
+    private List<String> positions;
+    private List<String> competitions;
+    private String strong_foot;
+    private String club;
+    private int kit_number;
+    private int wage;
+    private int market_value_19;
+    private int est_market_value_18;
+    private LocalDateTime contract_exp;
+    private int release_clause;
+    private int overall;
+    private int potential;
+    private String last_injury;
+
+    public static final Attribute NAME = new Attribute("Name");
+    public static final Attribute NATIONALITY = new Attribute("Nationality");
+    public static final Attribute CLUB = new Attribute("Club");
+    public static final Attribute BIRTHDATE = new Attribute("BirthDate");
+    public static final Attribute BIRTHPLACE = new Attribute("BirthPlace");
+    public static final Attribute POSITIONS = new Attribute("Positions");
+    public static final Attribute COMPETITIONS = new Attribute("Competitions");
+    public static final Attribute STRONGFOOT = new Attribute("StrongFoot");
+    public static final Attribute KITNUMBER = new Attribute("KitNumber");
+    public static final Attribute WAGE = new Attribute("Wage");
+    public static final Attribute MARKETVALUE19 = new Attribute("MarketValue19");
+    public static final Attribute ESTMARKETVALUE18 = new Attribute("EstMarketValue18");
+    public static final Attribute CONTRACTEXP = new Attribute("ContractExp");
+    public static final Attribute RELEASECLAUSE = new Attribute("ReleaseClause");
+    public static final Attribute OVERALL = new Attribute("Overall");
+    public static final Attribute POTENTIAL = new Attribute("Potential");
+    public static final Attribute LASTINJURY = new Attribute("LastInjury");
+
+    public Player(String identifier, String provenance) {
+        super(identifier, provenance);
+    }
+
+    @Override
+    public int hashCode() {
+        return getIdentifier().hashCode();
+    }
+
+    @Override
+    public boolean hasValue(Attribute attribute) {
+        if(attribute==NAME)
+            return name!=null;
+        else if(attribute==NATIONALITY)
+            return nationality!=null;
+        else if(attribute==CLUB)
+            return club!=null;
+        return false;
+    }
+
+    @Override
+    public String getIdentifier() {
+        // TODO Auto-generated method stub
+        return id;
+    }
+
+    @Override
+    public String getProvenance() {
+        // TODO Auto-generated method stub
+        return provenance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public List<String> getPositions() {
+        return positions;
+    }
+
+    public void setPositions(List<String> positions) {
+        this.positions = positions;
+    }
+
+    public String getStrong_foot() {
+        return strong_foot;
+    }
+
+    public void setStrong_foot(String strong_foot) {
+        this.strong_foot = strong_foot;
+    }
+
+    public LocalDateTime getBirth_date() {
+        return birth_date;
+    }
+
+    public void setBirth_date(LocalDateTime birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    public String getBirth_place() {
+        return birth_place;
+    }
+
+    public void setBirth_place(String birth_place) {
+        this.birth_place = birth_place;
+    }
+
+    public int getKit_number() {
+        return kit_number;
+    }
+
+    public void setKit_number(int kit_number) {
+        this.kit_number = kit_number;
+    }
+
+    public int getMarket_value_19() {
+        return market_value_19;
+    }
+
+    public void setMarket_value_19(int market_value_19) {
+        this.market_value_19 = market_value_19;
+    }
+
+    public LocalDateTime getContract_exp() {
+        return contract_exp;
+    }
+
+    public void setContract_exp(LocalDateTime contract_exp) {
+        this.contract_exp = contract_exp;
+    }
+
+    public String getClub() {
+        return club;
+    }
+
+    public void setClub(String club) {
+        this.club = club;
+    }
+
+    public int getWage() {
+        return wage;
+    }
+
+    public void setWage(int wage) {
+        this.wage = wage;
+    }
+
+    public int getRelease_clause() {
+        return release_clause;
+    }
+
+    public void setRelease_clause(int release_clause) {
+        this.release_clause = release_clause;
+    }
+
+    public int getOverall() {
+        return overall;
+    }
+
+    public void setOverall(int overall) {
+        this.overall = overall;
+    }
+
+    public int getPotential() {
+        return potential;
+    }
+
+    public void setPotential(int potential) {
+        this.potential = potential;
+    }
+
+    public String getLast_injury() {
+        return last_injury;
+    }
+
+    public void setLast_injury(String last_injury) {
+        this.last_injury = last_injury;
+    }
+
+
+    public List<String> getCompetitions() {
+        return competitions;
+    }
+
+    public void setCompetitions(List<String> competitions) {
+        this.competitions = competitions;
+    }
+
+    public int getEst_market_value_18() {
+        return est_market_value_18;
+    }
+
+    public void setEst_market_value_18(int est_market_value_18) {
+        this.est_market_value_18 = est_market_value_18;
+    }
+}
+
---
+++ src/main/java/model/PlayerXMLReader.java
@@ -0,0 +1,105 @@
+package model;
+
+import de.uni_mannheim.informatik.dws.winter.model.DataSet;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.model.io.XMLMatchableReader;
+import org.w3c.dom.Node;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.List;
+import java.util.Locale;
+
+public class PlayerXMLReader extends XMLMatchableReader<Player, Attribute> {
+
+    protected void initialiseDataset(DataSet<Player, Attribute> dataset) {
+        super.initialiseDataset(dataset);
+    }
+
+    @Override
+    public Player createModelFromElement(Node node, String provenanceInfo) {
+        String id = getValueFromChildElement(node, "id");
+
+        Player player = new Player(id, provenanceInfo);
+
+        // fill the attributes
+        player.setName(getValueFromChildElement(node, "name"));
+        player.setNationality(getValueFromChildElement(node, "nationality"));
+        player.setClub(getValueFromChildElement(node, "club"));
+
+        // convert the date string into a DateTime object
+        try {
+            String strong_foot = getValueFromChildElement(node, "strong_foot");
+            if (strong_foot != null && !strong_foot.isEmpty()) {
+                player.setStrong_foot(strong_foot);
+            }
+            String birth_place = getValueFromChildElement(node, "birth_place");
+            if (birth_place != null && !birth_place.isEmpty()) {
+                player.setBirth_place(birth_place);
+            }
+            String kit_number = getValueFromChildElement(node, "kit_number");
+            if (kit_number != null && !kit_number.isEmpty()) {
+                player.setKit_number(Integer.parseInt(kit_number));
+            }
+            String wage = getValueFromChildElement(node, "wage");
+            if (wage != null && !wage.isEmpty()) {
+                player.setWage(Integer.parseInt(wage));
+            }
+            String market_value_19 = getValueFromChildElement(node, "market_value_19");
+            if (market_value_19 != null && !market_value_19.isEmpty()) {
+                player.setMarket_value_19(Integer.parseInt(market_value_19));
+            }
+            String est_market_value_18 = getValueFromChildElement(node, "est_market_value_18");
+            if (est_market_value_18 != null && !est_market_value_18.isEmpty()) {
+                player.setEst_market_value_18(Integer.parseInt(est_market_value_18));
+            }
+            String release_clause = getValueFromChildElement(node, "release_clause");
+            if (release_clause != null && !release_clause.isEmpty()) {
+                player.setRelease_clause(Integer.parseInt(release_clause));
+            }
+            String overall = getValueFromChildElement(node, "overall");
+            if (overall != null && !overall.isEmpty()) {
+                player.setOverall(Integer.parseInt(overall));
+            }
+            String potential = getValueFromChildElement(node, "potential");
+            if (potential != null && !potential.isEmpty()) {
+                player.setPotential(Integer.parseInt(potential));
+            }
+            String last_injury = getValueFromChildElement(node, "last_injury");
+            if (last_injury != null && !last_injury.isEmpty()) {
+                player.setStrong_foot(last_injury);
+            }
+            String birth_date = getValueFromChildElement(node, "birth_date");
+            if (birth_date != null && !birth_date.isEmpty()) {
+                DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                        .appendPattern("yyyy-MM-dd").toFormatter(Locale.ENGLISH);
+                LocalDateTime dt = LocalDateTime.parse(birth_date, formatter);
+                player.setBirth_date(dt);
+            }
+            String contract_exp = getValueFromChildElement(node, "contract_exp");
+            if (contract_exp != null && !contract_exp.isEmpty()) {
+                DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                        .appendPattern("yyyy-MM-dd").toFormatter(Locale.ENGLISH);
+                LocalDateTime dt = LocalDateTime.parse(contract_exp, formatter);
+                player.setContract_exp(dt);
+            }
+
+            List<String> competitions = getListFromChildElement(node, "competitions");
+            if(competitions != null && competitions.isEmpty()) {
+                player.setCompetitions(competitions);
+            }
+
+            List<String> positions = getListFromChildElement(node, "positions");
+            if(positions != null && positions.isEmpty()) {
+                player.setPositions(positions);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return player;
+    }
+
+}
