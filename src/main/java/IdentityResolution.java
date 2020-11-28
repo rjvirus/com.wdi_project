e7369bb0137653d8dfaedd6ed8d1166b32eda542
rjvirus <rahulvirus100@gmail.com>
2020-11-28 15:21:50 +0100

    update Identity Resolution code; add comparators and blocker and output correspondes
    test run

--- src/main/java/IdentityResolution.java
+++ src/main/java/IdentityResolution.java
@@ -1,24 +1,54 @@
+import blocker.PlayerBlockingKeyByNationalityGenerator;
+import comparator.PlayerClubComparatorJaccard;
+import comparator.PlayerNameComparatorJaccard;
+import comparator.PlayerNationalityComparatorJaccard;
+import de.uni_mannheim.informatik.dws.winter.matching.MatchingEngine;
+import de.uni_mannheim.informatik.dws.winter.matching.blockers.StandardRecordBlocker;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
 import de.uni_mannheim.informatik.dws.winter.model.HashedDataSet;
 import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.matching.rules.LinearCombinationMatchingRule;
+import de.uni_mannheim.informatik.dws.winter.model.io.CSVCorrespondenceFormatter;
+import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+import de.uni_mannheim.informatik.dws.winter.utils.WinterLogManager;
 import model.Player;
 import model.PlayerXMLReader;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPathExpressionException;
+import org.slf4j.Logger;
 import java.io.File;
-import java.io.IOException;
 
 public class IdentityResolution {
-    public static void main(String args[]) throws ParserConfigurationException, SAXException, XPathExpressionException, IOException {
+    private static final Logger logger = WinterLogManager.activateLogger("default");
 
+    public static void main(String args[]) throws Exception {
+
+        //create a matching rule
+        LinearCombinationMatchingRule<Player, Attribute> matchingRule = new LinearCombinationMatchingRule<>(0.7);
+
+        //loading the data
         HashedDataSet<Player, Attribute> dataPredictionPlayers = new HashedDataSet<>();
         HashedDataSet<Player, Attribute> dataRealPlayers = new HashedDataSet<>();
         HashedDataSet<Player, Attribute> dataFifaPlayers = new HashedDataSet<>();
         new PlayerXMLReader().loadFromXML(new File("data/input/prediction_players.xml"),"/players/player", dataPredictionPlayers);
         new PlayerXMLReader().loadFromXML(new File("data/input/real_market_players.xml"),"/players/player", dataRealPlayers);
         new PlayerXMLReader().loadFromXML(new File("data/input/fifa_players.xml"),"/players/player", dataFifaPlayers);
-        
+
+        matchingRule.addComparator(new PlayerNameComparatorJaccard(), 0.5);
+        matchingRule.addComparator(new PlayerClubComparatorJaccard(), 0.15);
+        matchingRule.addComparator(new PlayerNationalityComparatorJaccard(), 0.35);
+
+        // Initialize Matching Engine
+        MatchingEngine<Player, Attribute> engine = new MatchingEngine<>();
+
+        // create a blocker (blocking strategy)
+        StandardRecordBlocker<Player, Attribute> blocker = new StandardRecordBlocker<Player, Attribute>(new PlayerBlockingKeyByNationalityGenerator());
+
+        // Execute the matching
+        Processable<Correspondence<Player, Attribute>> correspondences = engine.runIdentityResolution(
+                dataPredictionPlayers, dataRealPlayers, null, matchingRule, blocker);
+
+        // write the correspondences to the output file
+        new CSVCorrespondenceFormatter().writeCSV(new File("data/output/prediction_2_real_correspondences.csv"), correspondences);
+
         System.out.println("Hello world");
     }
 }
