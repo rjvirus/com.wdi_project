0201a62bd22da89b2cae7d5004d4380dd460af99
rjvirus <rahulvirus100@gmail.com>
2020-11-29 23:19:02 +0100

    fix prediction to real -> real to prediction

--- src/main/java/IdentityResolution.java
+++ src/main/java/IdentityResolution.java
@@ -20,22 +20,22 @@ public class IdentityResolution {
     public static void main(String args[]) throws Exception {
 
         //create 3 matching rules
-        LinearCombinationMatchingRule<Player, Attribute> matchingRulePredReal = new LinearCombinationMatchingRule<>(0.7);
+        LinearCombinationMatchingRule<Player, Attribute> matchingRuleRealPred = new LinearCombinationMatchingRule<>(0.7);
         LinearCombinationMatchingRule<Player, Attribute> matchingRuleFifaReal = new LinearCombinationMatchingRule<>(0.5);
 
         //loading the data
-        HashedDataSet<Player, Attribute> dataPredictionPlayers = new HashedDataSet<>();
         HashedDataSet<Player, Attribute> dataRealPlayers = new HashedDataSet<>();
+        HashedDataSet<Player, Attribute> dataPredictionPlayers = new HashedDataSet<>();
         HashedDataSet<Player, Attribute> dataFifaPlayers = new HashedDataSet<>();
-        new PlayerXMLReader().loadFromXML(new File("data/input/prediction_players.xml"),"/players/player", dataPredictionPlayers);
         new PlayerXMLReader().loadFromXML(new File("data/input/real_market_players.xml"),"/players/player", dataRealPlayers);
+        new PlayerXMLReader().loadFromXML(new File("data/input/prediction_players.xml"),"/players/player", dataPredictionPlayers);
         new PlayerXMLReader().loadFromXML(new File("data/input/fifa_players.xml"),"/players/player", dataFifaPlayers);
 
         //added comparators for PredReal
-        matchingRulePredReal.addComparator(new PlayerNameComparatorJaccard(), 0.40);
-        matchingRulePredReal.addComparator(new PlayerClubComparatorJaccard(), 0.10);
-        matchingRulePredReal.addComparator(new PlayerNationalityComparatorJaccard(), 0.25);
-        matchingRulePredReal.addComparator(new PlayerBirthDateComparatorEqual(), 0.25);
+        matchingRuleRealPred.addComparator(new PlayerNameComparatorJaccardTokenizer(), 0.40);
+        matchingRuleRealPred.addComparator(new PlayerClubComparatorJaccard(), 0.10);
+        matchingRuleRealPred.addComparator(new PlayerNationalityComparatorJaccard(), 0.25);
+        matchingRuleRealPred.addComparator(new PlayerBirthDateComparatorEqual(), 0.25);
 
         //added comparators for FifaReal
         matchingRuleFifaReal.addComparator(new PlayerNameComparatorJaccard(), 0.6);
@@ -51,14 +51,14 @@ public class IdentityResolution {
         StandardRecordBlocker<Player, Attribute> blocker = new StandardRecordBlocker<Player, Attribute>(new PlayerBlockingKeyByNationalityGenerator());
 
         // Execute the matchings
-        Processable<Correspondence<Player, Attribute>> correspondencesPredReal = enginePredReal.runIdentityResolution(
-                dataPredictionPlayers, dataRealPlayers, null, matchingRulePredReal, blocker);
+        Processable<Correspondence<Player, Attribute>> correspondencesRealPred = enginePredReal.runIdentityResolution(
+                dataRealPlayers, dataPredictionPlayers, null, matchingRuleRealPred, blocker);
         Processable<Correspondence<Player, Attribute>> correspondencesFifaReal = engineFifaReal.runIdentityResolution(
                 dataFifaPlayers, dataRealPlayers, null, matchingRuleFifaReal, blocker);
 
 
         // write the correspondences to the output file
-        new CSVCorrespondenceFormatter().writeCSV(new File("data/output/prediction_2_real_correspondences.csv"), correspondencesPredReal);
+        new CSVCorrespondenceFormatter().writeCSV(new File("data/output/real_2_prediction_correspondences.csv"), correspondencesRealPred);
         new CSVCorrespondenceFormatter().writeCSV(new File("data/output/fifa_2_real_correspondences.csv"), correspondencesFifaReal);
 
         System.out.println("Hello world");
