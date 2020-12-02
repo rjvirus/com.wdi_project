53f45f98d603022717406b3ba10b3ad143bf0fac
rjvirus <rahulvirus100@gmail.com>
2020-12-02 23:47:14 +0100

    Create PlayerNameShortComparatorJaccard.java
    modify long names to short forms by cutting the first name to <1stLetter>+'.' + '<space>' + <last name>

---
+++ src/main/java/comparator/PlayerNameShortComparatorJaccard.java
@@ -0,0 +1,51 @@
+package comparator;
+
+import de.uni_mannheim.informatik.dws.winter.matching.rules.Comparator;
+import de.uni_mannheim.informatik.dws.winter.matching.rules.ComparatorLogger;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.similarity.string.JaccardOnNGramsSimilarity;
+import model.Player;
+
+public class PlayerNameShortComparatorJaccard implements Comparator<Player, Attribute> {
+
+    private static final long serialVersionUID = 1L;
+    JaccardOnNGramsSimilarity sim = new JaccardOnNGramsSimilarity(3);
+
+    private ComparatorLogger comparisonLog;
+
+    @Override
+    public double compare(Player record1, Player record2, Correspondence<Attribute, Matchable> schemaCorrespondence) {
+        String s1 = record1.getName();
+        String s2 = record2.getName();
+
+        String[] s1Parts = s1.split(" ");
+        System.out.println(s1Parts[0].charAt(0));
+        String modR1 = s1Parts[0].charAt(0) + "." + " " + s1Parts[s1Parts.length-1];
+
+        double similarity = sim.calculate(modR1, s2);
+
+        if(this.comparisonLog != null){
+            this.comparisonLog.setComparatorName(getClass().getName());
+
+            this.comparisonLog.setRecord1Value(modR1);
+            this.comparisonLog.setRecord2Value(s2);
+
+            this.comparisonLog.setSimilarity(Double.toString(similarity));
+        }
+
+        return similarity;
+    }
+
+    @Override
+    public ComparatorLogger getComparisonLog() {
+        return this.comparisonLog;
+    }
+
+    @Override
+    public void setComparisonLog(ComparatorLogger comparatorLog) {
+        this.comparisonLog = comparatorLog;
+    }
+
+}
