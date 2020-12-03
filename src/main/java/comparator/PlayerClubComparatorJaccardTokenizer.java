ca52541a2b49fb1a59e1ccd3c6041fcd4f7cb183
rjvirus <rahulvirus100@gmail.com>
2020-12-03 01:55:39 +0100

    Create PlayerClubComparatorJaccardTokenizer.java

---
+++ src/main/java/comparator/PlayerClubComparatorJaccardTokenizer.java
@@ -0,0 +1,45 @@
+package comparator;
+
+import de.uni_mannheim.informatik.dws.winter.matching.rules.Comparator;
+import de.uni_mannheim.informatik.dws.winter.matching.rules.ComparatorLogger;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.similarity.string.TokenizingJaccardSimilarity;
+import model.Player;
+
+public class PlayerClubComparatorJaccardTokenizer implements Comparator<Player, Attribute> {
+    private static final long serialVersionUID = 1L;
+    TokenizingJaccardSimilarity sim = new TokenizingJaccardSimilarity();
+
+    private ComparatorLogger comparisonLog;
+
+    @Override
+    public double compare(Player record1, Player record2, Correspondence<Attribute, Matchable> schemaCorrespondence) {
+        String s1 = record1.getClub();
+        String s2 = record2.getClub();
+
+        double similarity = sim.calculate(s1, s2);
+
+        if(this.comparisonLog != null){
+            this.comparisonLog.setComparatorName(getClass().getName());
+
+            this.comparisonLog.setRecord1Value(s1);
+            this.comparisonLog.setRecord2Value(s2);
+
+            this.comparisonLog.setSimilarity(Double.toString(similarity));
+        }
+
+        return similarity;
+    }
+
+    @Override
+    public ComparatorLogger getComparisonLog() {
+        return this.comparisonLog;
+    }
+
+    @Override
+    public void setComparisonLog(ComparatorLogger comparatorLog) {
+        this.comparisonLog = comparatorLog;
+    }
+}
