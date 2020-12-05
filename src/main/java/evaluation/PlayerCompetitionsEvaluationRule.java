9b61aa73ed2c6a72ebc760254701e21b8b733211
rjvirus <rahulvirus100@gmail.com>
2020-12-05 02:00:44 +0100

    add competitions fuser and evaluater

---
+++ src/main/java/evaluation/PlayerCompetitionsEvaluationRule.java
@@ -0,0 +1,40 @@
+package evaluation;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+import de.uni_mannheim.informatik.dws.winter.datafusion.EvaluationRule;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import model.Player;
+
+public class PlayerCompetitionsEvaluationRule extends EvaluationRule<Player, Attribute>{
+
+    @Override
+    public boolean isEqual(Player record1, Player record2, Attribute schemaElement) {
+        Set<String> actors1 = new HashSet<>();
+        if (record1.getCompetitions() != null) {
+            for (String p : record1.getCompetitions()) {
+
+                actors1.add(p);
+            }
+        }
+        Set<String> actors2 = new HashSet<>();
+        if (record2.getCompetitions() != null) {
+            for (String p : record2.getCompetitions()) {
+                actors2.add(p);
+            }
+        }
+
+        return actors1.containsAll(actors2) && actors2.containsAll(actors1);
+    }
+
+    @Override
+    public boolean isEqual(Player record1, Player record2,
+                           Correspondence<Attribute, Matchable> schemaCorrespondence) {
+        return isEqual(record1, record2, (Attribute)null);
+    }
+}
+
---
+++ src/main/java/fusers/PlayerCompetitionsFuser.java
@@ -0,0 +1,40 @@
+package fusers;
+
+import de.uni_mannheim.informatik.dws.winter.datafusion.AttributeValueFuser;
+import de.uni_mannheim.informatik.dws.winter.datafusion.conflictresolution.list.Union;
+import de.uni_mannheim.informatik.dws.winter.matching.rules.Comparator;
+import de.uni_mannheim.informatik.dws.winter.matching.rules.ComparatorLogger;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.FusedValue;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.RecordGroup;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+import de.uni_mannheim.informatik.dws.winter.similarity.string.TokenizingJaccardSimilarity;
+import model.Player;
+
+import java.util.List;
+
+public class PlayerCompetitionsFuser extends AttributeValueFuser<List<String>, Player, Attribute> {
+    public PlayerCompetitionsFuser() {
+        super(new Union<String, Player, Attribute>());
+    }
+    @Override
+    public boolean hasValue(Player record, Correspondence<Attribute, Matchable> correspondence) {
+        return record.hasValue(Player.COMPETITIONS);
+    }
+
+    @Override
+    public List<String> getValue(Player record, Correspondence<Attribute, Matchable> correspondence) {
+        return record.getCompetitions();
+    }
+
+    @Override
+    public void fuse(RecordGroup<Player, Attribute> group, Player fusedRecord, Processable<Correspondence<Attribute, Matchable>> schemaCorrespondences, Attribute schemaElement) {
+        FusedValue<List<String>, Player, Attribute> fused = getFusedValue(group, schemaCorrespondences, schemaElement);
+        fusedRecord.setCompetitions(fused.getValue());
+        fusedRecord.setAttributeProvenance(Player.COMPETITIONS, fused.getOriginalIds());
+    }
+
+
+}
